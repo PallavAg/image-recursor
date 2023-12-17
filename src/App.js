914ad68b6274f8d409ad884d58f819538b28ceb8
pallavag <agpallav@gmail.com>
2023-12-18 07:35:19 +0800

    Update UI, Add dropdown

--- src/App.js
+++ src/App.js
@@ -3,20 +3,35 @@ import "./App.css";
 import gifshot from "gifshot";
 import OpenAI from "openai";
 import "./firebase.js";
+import Cookies from "js-cookie";
 
 const API_KEY = "";
-const PATH1 =
-  "m72.97,56.78l-15.81-6.9c-3.82-1.67-6.87-4.72-8.54-8.54l-6.9-15.81c-.54-1.23-2.27-1.23-2.81,0l-6.9,15.81c-1.67,3.82-4.72,6.87-8.54,8.54l-15.84,6.91c-1.22.53-1.23,2.27,0,2.81l16.11,7.12c3.81,1.69,6.85,4.75,8.5,8.58l6.68,15.51c.53,1.23,2.28,1.24,2.81,0l6.89-15.79c1.67-3.82,4.72-6.87,8.54-8.54l15.81-6.9c1.23-.54,1.23-2.27,0-2.81Z";
-const PATH2 =
-  "m92.76,26.84l-9.14-3.99c-2.21-.96-3.97-2.73-4.93-4.93l-3.99-9.14c-.31-.71-1.31-.71-1.62,0l-3.99,9.14c-.96,2.21-2.73,3.97-4.93,4.93l-9.15,3.99c-.71.31-.71,1.31,0,1.62l9.31,4.12c2.2.97,3.96,2.75,4.91,4.96l3.86,8.96c.31.71,1.32.71,1.63,0l3.98-9.12c.96-2.21,2.73-3.97,4.93-4.93l9.14-3.99c.71-.31.71-1.31,0-1.62Z";
 
 const openai = new OpenAI({
   apiKey: API_KEY,
   dangerouslyAllowBrowser: true,
 });
 
+const promptTemplates = {
+  Default:
+    "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt.",
+  "Make it scarier":
+    "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt. BUT MAKE THE PROMPT SCARIER.",
+  "Make it more random":
+    "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt. FOR SOME FUN, ADD RANDOM DETAILS IN THE PROMPT.",
+  "Make it more mystical":
+    "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt. BUT MAKE THE PROMPT MORE MYSTICAL.",
+  "Make it more intense":
+    "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt. BUT MAKE THE DETAILS IN THE PROMPT MORE INTENSE AND EXTREME.",
+  "Make it funnier":
+    "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt. BUT MAKE THE PROMPT FUNNIER.",
+};
+
 function App() {
   const [prompt, setPrompt] = useState("");
+  const [visionPrompt, setVisionPrompt] = useState(promptTemplates["Default"]);
+  const [selectedOption, setSelectedOption] = useState("Default");
+  const [customPromptSelected, setCustomPromptSelected] = useState(false);
   const [imageCount, setImageCount] = useState(10);
   const [apiKey, setApiKey] = useState(API_KEY);
   const [loadingText, setLoadingText] = useState("");
@@ -24,28 +39,6 @@ function App() {
   const [isComplete, setIsComplete] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
   const [results, setResults] = useState([]);
-  // const [results, setResults] = useState([
-  //   {
-  //     content:
-  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-  //     isImage: true,
-  //   },
-  //   {
-  //     content:
-  //       "This is the prompt used to generate the image above that I will use to generate the dalle image",
-  //     isImage: false,
-  //   },
-  //   {
-  //     content:
-  //       "https://i.imgur.com/Jx9FWK2_d.webp?maxwidth=520&shape=thumb&fidelity=high",
-  //     isImage: true,
-  //   },
-  //   {
-  //     content:
-  //       "This is the prompt used to generate the image above that I will use to generate the dalle image",
-  //     isImage: false,
-  //   },
-  // ]);
 
   const handleAboutClick = () => {
     setShowAbout(!showAbout);
@@ -57,17 +50,38 @@ function App() {
         <h2 className="font-bold text-2xl pb-1">About Image Recursor</h2>
         <div className="text-xs sm:text-lg md:text-lg lg:text-lg">
           <p>Write a starting prompt and see how it changes over time!</p>
-          <p>Your API Key always stays on-device</p>
+          <p>
+            Your API Key always stays on-device.{" "}
+            <a
+              href="https://platform.openai.com/account/api-keys"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <u>
+                <b>Get one here</b>
+              </u>
+            </a>
+            .
+          </p>
         </div>
-        {/* Follow me on twitter button */}
-        <a
-          href="https://twitter.com/pallavmac"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="inline-block border-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4"
-        >
-          Follow Me on Twitter
-        </a>
+        <div className="space-x-4 flex justify-center items-center">
+          <a
+            href="https://twitter.com/pallavmac"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block border border-white/80 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4"
+          >
+            Follow Me on Twitter
+          </a>
+          <a
+            href="https://github.com/PallavAg/image-recursor"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block border border-white/80 bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-lg mt-4"
+          >
+            View Project on GitHub
+          </a>
+        </div>
       </div>
     );
   };
@@ -101,6 +115,14 @@ function App() {
   }
 
   useEffect(() => {
+    const storedApiKey = Cookies.get("apiKey");
+    if (storedApiKey) {
+      setApiKey(storedApiKey);
+      openai.apiKey = storedApiKey;
+    }
+  }, []);
+
+  useEffect(() => {
     if (isComplete) {
       createGIF();
     }
@@ -115,6 +137,7 @@ function App() {
       alert("An API Key is needed to begin.");
       return;
     }
+    Cookies.set("apiKey", apiKey);
     console.log(imageCount);
     if (!imageCount || imageCount < 1) {
       alert("Image count must be greater than 0.");
@@ -138,7 +161,7 @@ function App() {
         response_format: "b64_json",
       });
       let image_b64 = `data:image/png;base64,${imageResponse.data[0].b64_json}`;
-      console.log("Image URL Len:", image_b64.length);
+      console.log("Image b64 Len:", image_b64.length);
       setResults((prevResults) => [
         { content: image_b64, isImage: true },
         { content: "Loading...", isImage: false },
@@ -157,7 +180,7 @@ function App() {
             content: [
               {
                 type: "text",
-                text: "Describe this image. The output will be used to generate the prompt on DALL-E so output only the DALL-E prompt. To add some fun, describe something random that doesn't exist in the image.",
+                text: visionPrompt,
               },
               {
                 type: "image_url",
@@ -182,6 +205,19 @@ function App() {
     setIsComplete(true);
     setLoadingText("");
   }
+
+  const handleSelectChange = (e) => {
+    const selectedKey = e.target.value;
+    setSelectedOption(selectedKey);
+
+    if (selectedKey === "custom") {
+      setCustomPromptSelected(true);
+      setVisionPrompt(promptTemplates["Default"]);
+    } else {
+      setCustomPromptSelected(false);
+      setVisionPrompt(promptTemplates[selectedKey]);
+    }
+  };
 
   function ResultsList() {
     // Filter out pairs of image and text
@@ -242,12 +278,12 @@ function App() {
         </div>
       </nav>
       {showAbout && aboutContent()}
-      <div className="flex min-h-screen flex-col items-center bg-gradient-to-br from-gray-950 to-blue-950 pt-6 font-sans text-white">
+      <div className="flex min-h-screen flex-col items-center bg-cross-gradient pt-6 font-sans text-white">
         <h1 className="text-3xl sm:text-5xl md:text-5xl lg:text-5xl p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 mb-6">
           DALL-E 3 & GPT-4 Vision <br /> Image Recursor
         </h1>
-        <div className="bg-gradient-to-br from-zinc-950 to-neutral-900 rounded-xl w-lg p-4 mb-4 mx-2 shadow-md shadow-gray-900 text-left border border-white">
-          <div className="mb-4">
+        <div className="bg-black/20 backdrop-blur-md rounded-xl w-lg p-4 mb-4 mx-2 shadow-lg shadow-gray-900/50 border border-black/50 text-left">
+          <div className="mb-2">
             <label
               htmlFor="prompt"
               className="mb-2 block text-sm font-semibold text-gray-200 text-left"
@@ -256,9 +292,9 @@ function App() {
             </label>
             <textarea
               id="prompt"
-              placeholder="Choose a starting image prompt"
-              className="w-full rounded-lg border border-gray-700 bg-gray-900 p-2 text-white shadow-md focus:outline-none overflow-hidden"
-              style={{ maxHeight: "3.5rem" }} // This height is roughly two lines of text, adjust as necessary for your font size.
+              placeholder="A unicorn driving a car on the moon..."
+              className="w-full rounded-lg bg-gray-900 p-2 text-white shadow-md focus:outline-none overflow-hidden"
+              style={{ maxHeight: "3.5rem" }}
               value={prompt}
               onChange={(e) => setPrompt(e.target.value)}
               autoComplete="off"
@@ -278,7 +314,7 @@ function App() {
               <input
                 id="count"
                 type="number"
-                className="w-full rounded-lg border border-gray-700 bg-gray-900 p-2 text-white shadow-md focus:outline-none"
+                className="w-full rounded-lg bg-gray-900 p-2 text-white shadow-md focus:outline-none"
                 value={imageCount}
                 autoComplete="off"
                 min="1"
@@ -311,7 +347,7 @@ function App() {
                     </svg>
                   </button>
                   <div className="absolute bottom-full mb-2 hidden group-hover:block w-48 rounded-md bg-gray-800 p-2 text-sm text-gray-100">
-                    Your API key stays on-device
+                    Your API key stays on-device.
                   </div>
                 </div>
               </div>
@@ -319,7 +355,7 @@ function App() {
                 id="api-key"
                 type="password"
                 placeholder="sk-123..."
-                className="w-full rounded-lg border border-gray-700 bg-gray-900 p-2 -mt-1 text-white shadow-md focus:outline-none"
+                className="w-full rounded-lg bg-gray-900 p-2 -mt-1 text-white shadow-md focus:outline-none"
                 value={apiKey.replace(/./g, "●")}
                 onChange={(e) => {
                   openai.apiKey = e.target.value;
@@ -328,9 +364,41 @@ function App() {
               />
             </div>
           </div>
-          <p className="text-gray-400 text-xs mt-4">
-            Gartic phone for images: Write a prompt and see how it changes over
-            time!
+          {/* GPT-4 Vision Prompt */}
+          <div className="mt-4">
+            <label
+              htmlFor="visionPrompt"
+              className="mb-2 block text-sm font-semibold text-gray-200 text-left"
+            >
+              GPT-4 Vision Prompt
+            </label>
+            <select
+              id="visionPromptSelect"
+              className="w-full rounded-lg bg-gray-900 p-2 text-white shadow-md focus:outline-none"
+              value={selectedOption}
+              onChange={handleSelectChange}
+            >
+              {Object.entries(promptTemplates).map(([key]) => (
+                <option key={key} value={key}>
+                  {key}
+                </option>
+              ))}
+              <option value="custom">Custom Prompt</option>
+            </select>
+            {customPromptSelected && (
+              <textarea
+                id="visionPrompt"
+                placeholder="Write a custom prompt for GPT-4 Vision..."
+                className="w-full mt-2 rounded-lg bg-gray-900 p-2 text-white shadow-md focus:outline-none"
+                value={visionPrompt}
+                onChange={(e) => setVisionPrompt(e.target.value)}
+                rows="3"
+              />
+            )}
+          </div>
+          <p className="text-gray-400 text-xs mt-3">
+            <b>Gartic phone for images:</b> Write a prompt and see how it
+            changes over time!
           </p>
         </div>
         <button
@@ -386,27 +454,9 @@ function App() {
   );
 }
 
-// {gifURL && (
-//   <div className="max-w-lg p-4 w-full text-center">
-//     <h1 className="font-black text-3xl pb-1">Result ✨</h1>
-//     <img src={gifURL} alt="Generated GIF" className="rounded-sm" />
-//   </div>
-// )}
-// <div className="my-4">{ResultsList()}</div>
-export default App;
+const PATH1 =
+  "m72.97,56.78l-15.81-6.9c-3.82-1.67-6.87-4.72-8.54-8.54l-6.9-15.81c-.54-1.23-2.27-1.23-2.81,0l-6.9,15.81c-1.67,3.82-4.72,6.87-8.54,8.54l-15.84,6.91c-1.22.53-1.23,2.27,0,2.81l16.11,7.12c3.81,1.69,6.85,4.75,8.5,8.58l6.68,15.51c.53,1.23,2.28,1.24,2.81,0l6.89-15.79c1.67-3.82,4.72-6.87,8.54-8.54l15.81-6.9c1.23-.54,1.23-2.27,0-2.81Z";
+const PATH2 =
+  "m92.76,26.84l-9.14-3.99c-2.21-.96-3.97-2.73-4.93-4.93l-3.99-9.14c-.31-.71-1.31-.71-1.62,0l-3.99,9.14c-.96,2.21-2.73,3.97-4.93,4.93l-9.15,3.99c-.71.31-.71,1.31,0,1.62l9.31,4.12c2.2.97,3.96,2.75,4.91,4.96l3.86,8.96c.31.71,1.32.71,1.63,0l3.98-9.12c.96-2.21,2.73-3.97,4.93-4.93l9.14-3.99c.71-.31.71-1.31,0-1.62Z";
 
-/* 
-Todo
-- Start with an image url
-- Advanced options (HD, Prompt etc.)
-- Remove console output
-- Share GIF link
-- Keep producing button
-- Error Handling - DONE
-- About Tab? - DONE
-- Hide API Key - DONE
-- Use table input format? - DONE
-- Stop on error - Done
-- Download GIF - DONE
-- OpenAI Info Button - DONE
-*/
+export default App;
